dadc2550f049a9e2df81af1f2869be798397d54c
Sourabh <84093317+sourabh4618@users.noreply.github.com>
2023-12-04 12:50:12 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -16,13 +16,7 @@ public class Main {
         int ratedCapacity = 120;
 
         for (int presentCapacity : presentCapacities) {
-            // Ensure present capacity is non-negative
-            if (presentCapacity < 0) {
-                throw new IllegalArgumentException("Present capacity cannot be negative.");
-            }
-            if (presentCapacity > 120) {
-                throw new IllegalArgumentException("Present capacity cannot be greater than 120.");
-            }
+            
 
             // Compute SoH percentage
             double soh = (double) ((presentCapacity / ratedCapacity) * 100);
@@ -42,39 +36,14 @@ public class Main {
 
     static void testBucketingByHealth() {
         System.out.println("Counting batteries by SoH...\n");
-
-        // Test case with present capacities
-        int[] presentCapacities = {113, 116, 80, 95, 92, 70};
+        //sample test case
+        int[] presentCapacities = {113, 116, 80, 95, 92, 70};           
         CountsBySoH counts = countBatteriesByHealth(presentCapacities);
-
-        // Asserts to validate the counts
-        assert counts.healthy == 2;
-        assert counts.exchange == 3;
-        assert counts.failed == 1;
-
-        // Additional test cases with boundary conditions
-        int[] edgeCase1 = {120};  // Single battery with maximum rated capacity
-        CountsBySoH countsEdge1 = countBatteriesByHealth(edgeCase1);
-        assert countsEdge1.healthy == 1;
-        assert countsEdge1.exchange == 0;
-        assert countsEdge1.failed == 0;
-
-        int[] edgeCase2 = {0};  // Single battery with minimum present capacity
-        CountsBySoH countsEdge2 = countBatteriesByHealth(edgeCase2);
-        assert countsEdge2.healthy == 0;
-        assert countsEdge2.exchange == 0;
-        assert countsEdge2.failed == 1;
-
-        int[] edgeCase3 = {-10, 130};  // Batteries with negative, valid, and invalid capacities
-        try {
-            countBatteriesByHealth(edgeCase3);
-        } catch (IllegalArgumentException e) {
-            System.out.println("IllegalArgumentException caught: " + e.getMessage());
-        }
-
-        System.out.println("Done counting \n");
+        assert(counts.healthy == 2);
+        assert(counts.exchange == 3);
+        assert(counts.failed == 1);
+        System.out.println("Done counting :)\n");
     }
-
     public static void main(String[] args) {
         testBucketingByHealth();
     }
