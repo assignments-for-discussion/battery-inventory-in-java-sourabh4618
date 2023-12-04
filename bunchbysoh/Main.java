4a1bb5d1b53aef4a6dd7ff7101500605250fe728
Sourabh <84093317+sourabh4618@users.noreply.github.com>
2023-12-04 12:59:01 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -10,42 +10,40 @@ public class Main {
     }
 
     static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
-        CountsBySoH counts = new CountsBySoH();
+      CountsBySoH counts = new CountsBySoH();
 
-        // Rated capacity of a new battery
-        int ratedCapacity = 120;
+      // Rated capacity of a new battery
+      int ratedCapacity = 120;
 
-        for (int presentCapacity : presentCapacities) {
+      for (int j=0;j<presentCapacities.length;j++) {
             
+        // Compute SoH percentage
+        double soh = (double) ((presentCapacities[j] / ratedCapacity) * 100);
 
-            // Compute SoH percentage
-            double soh = (double) ((presentCapacity / ratedCapacity) * 100);
-
-            // Classify batteries based on SoH
-            if (soh >= 80 && soh <= 100) {
-                counts.healthy++;
-            } else if (soh >= 62 && soh < 80) {
-                counts.exchange++;
-            } else {
-                counts.failed++;
-            }
+        // Classify batteries based on SoH
+        if (soh >= 80 && soh <= 100) {
+          counts.healthy++;
+        } else if (soh >= 62 && soh < 80) {
+          counts.exchange++;
+        } else {
+          counts.failed++;
         }
-
-        return counts;
+      }
+      return counts;
     }
 
     static void testBucketingByHealth() {
-        System.out.println("Counting batteries by SoH...\n");
-        //sample test case
-        int[] presentCapacities = {113, 116, 80, 95, 92, 70};           
-        CountsBySoH counts = countBatteriesByHealth(presentCapacities);
-        assert(counts.healthy == 2);
-        assert(counts.exchange == 3);
-        assert(counts.failed == 1);
-        System.out.println("Done counting :)\n");
+      System.out.println("Counting batteries by SoH...\n");
+      //sample test cases
+      int[] presentCapacities = {113, 116, 80, 95, 92, 70, 60};           
+      CountsBySoH counts = countBatteriesByHealth(presentCapacities);
+      assert(counts.healthy == 2);
+      assert(counts.exchange == 3);
+      assert(counts.failed == 2);
+      System.out.println("Done counting :)\n");
     }
     public static void main(String[] args) {
-        testBucketingByHealth();
+      testBucketingByHealth();
     }
 }
 
