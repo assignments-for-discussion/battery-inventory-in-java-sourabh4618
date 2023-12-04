42e90579f70559efdc84e6662812c05b3ada2b0a
Sourabh <84093317+sourabh4618@users.noreply.github.com>
2023-12-04 13:16:20 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -15,7 +15,7 @@ public class Main {
     for (int j=0;j<presentCapacities.length;j++) {
             
       // Compute SoH percentage
-      double soh = (double) ((presentCapacities[j] / ratedCapacity) * 100);
+      double soh = (double) (presentCapacities[j] / ratedCapacity * 100);
 
       // Classify batteries based on SoH
       if (soh >= 80 && soh <= 100) {
@@ -33,9 +33,7 @@ public class Main {
     System.out.println("Counting batteries by SoH...\n");
     int[] presentCapacities = {113, 116, 80, 95, 92, 70};           //sample test case
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
-    assert(counts.healthy == 2);
-    assert(counts.exchange == 3);
-    assert(counts.failed == 1);
+    
     System.out.println("Done counting :)\n");
   }
   public static void main(String[] args) {
